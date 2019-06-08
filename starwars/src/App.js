8d0a328a3f6fdfd826ecc969e6b82d45d8377595
teej387 <teej387@yahoo.com>
2019-06-07 22:27:07 -0500

    Added more elements to Character

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Character from './components/Character'
+
 import './App.css';
 
 class App extends Component {
@@ -9,7 +11,7 @@ class App extends Component {
       name: '',
       gender: '',
       species: [],
-      vehicles: []
+      homeworld: ''
     };
   }
 
@@ -18,9 +20,7 @@ class App extends Component {
   }
 
   getCharacters = URL => {
-    // feel free to research what this code is doing.
-    // At a high level we are calling an API to fetch some starwars data from the open web.
-    // We then take that data and resolve it our state.
+  
     fetch(URL)
       .then(res => {
         return res.json();
--- starwars/src/components/character.js
+++ starwars/src/components/character.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
 function Character(props) {
-  
+
   return (
-    <div className='character'>
-      <h2></h2>
+    <div className="character">
+      <h3>{props.character.name}</h3>
+      <p>
+        <strong>Gender: </strong> {props.character.gender}
+      </p>
+      <p>
+        <strong>Species: </strong>
+      </p>
+      <p>
+        <strong>Home World: </strong> {props.character.homeworld}
+      </p>
     </div>
-  )
+  );
 };
 
 export default Character;
